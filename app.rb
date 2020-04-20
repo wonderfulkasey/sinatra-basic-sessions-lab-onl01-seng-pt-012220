a720a46d720684afe0d56cde8e4330bbc14add3f
Kasey Arnold <kaseyarnold15@gmail.com>
2020-04-20 20:50:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,25 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  require_relative 'config/environment'
+
+  class App < Sinatra::Base
+
+    configure do
+      enable :sessions
+      set :session_secret, 'password'
+    end
+
+    get '/' do
+      erb :index
+    end
+
+    post '/checkout' do
+      session[:item] = params[:item]
+      @session = session
+      erb :checkout
+    end
+  end
+
 end
